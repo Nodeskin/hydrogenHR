59949e9ce4a9cfe16c40d0063e76953f5cb9ad70
nodeSkin <shittujamiu@gmail.com>
2023-05-25 22:36:17 +0100

    HyrdrogenHR updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector(".menu-icon");
 const close = document.querySelector(".close-icon");
 const menuNav = document.querySelector(".one");
-const menuDropDown = document.querySelector(".hamburgMid-nav")
+const menuDropDown = document.querySelector(".hamburgMid-nav");
 
 menu.addEventListener("click", () => {
   menu.style.display = "none";
